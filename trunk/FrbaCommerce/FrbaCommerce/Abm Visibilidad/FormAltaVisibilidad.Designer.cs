4f65a7430d9710376229bbd231c8c888802d0692
mendezbruno2014 <mendezbruno2014@5bc95602-4d4b-4591-9177-1204e55da58d>
2014-06-05 04:25:15 +0000

    ABM visibilidad

---
+++ trunk/FrbaCommerce/FrbaCommerce/Abm Visibilidad/FormAltaVisibilidad.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FrbaCommerce.Abm_Visibilidad
+{
+    partial class FormAltaVisibilidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCodigo = new System.Windows.Forms.TextBox();
+            this.textBoxDescripcion = new System.Windows.Forms.TextBox();
+            this.textBoxPPP = new System.Windows.Forms.TextBox();
+            this.textBoxPorcentaje = new System.Windows.Forms.TextBox();
+            this.buttonAlta = new System.Windows.Forms.Button();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Precio Por Publicar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Porcentaje Por Venta";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descripcion";
+            // 
+            // textBoxCodigo
+            // 
+            this.textBoxCodigo.Location = new System.Drawing.Point(163, 43);
+            this.textBoxCodigo.Name = "textBoxCodigo";
+            this.textBoxCodigo.Size = new System.Drawing.Size(106, 20);
+            this.textBoxCodigo.TabIndex = 4;
+            // 
+            // textBoxDescripcion
+            // 
+            this.textBoxDescripcion.Location = new System.Drawing.Point(163, 75);
+            this.textBoxDescripcion.Name = "textBoxDescripcion";
+            this.textBoxDescripcion.Size = new System.Drawing.Size(106, 20);
+            this.textBoxDescripcion.TabIndex = 5;
+            // 
+            // textBoxPPP
+            // 
+            this.textBoxPPP.Location = new System.Drawing.Point(163, 114);
+            this.textBoxPPP.Name = "textBoxPPP";
+            this.textBoxPPP.Size = new System.Drawing.Size(106, 20);
+            this.textBoxPPP.TabIndex = 6;
+            // 
+            // textBoxPorcentaje
+            // 
+            this.textBoxPorcentaje.Location = new System.Drawing.Point(163, 154);
+            this.textBoxPorcentaje.Name = "textBoxPorcentaje";
+            this.textBoxPorcentaje.Size = new System.Drawing.Size(106, 20);
+            this.textBoxPorcentaje.TabIndex = 7;
+            // 
+            // buttonAlta
+            // 
+            this.buttonAlta.Location = new System.Drawing.Point(183, 223);
+            this.buttonAlta.Name = "buttonAlta";
+            this.buttonAlta.Size = new System.Drawing.Size(97, 31);
+            this.buttonAlta.TabIndex = 8;
+            this.buttonAlta.Text = "Alta";
+            this.buttonAlta.UseVisualStyleBackColor = true;
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(18, 223);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(97, 31);
+            this.buttonLimpiar.TabIndex = 9;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // FormAltaVisibilidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.buttonAlta);
+            this.Controls.Add(this.textBoxPorcentaje);
+            this.Controls.Add(this.textBoxPPP);
+            this.Controls.Add(this.textBoxDescripcion);
+            this.Controls.Add(this.textBoxCodigo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAltaVisibilidad";
+            this.Text = "Alta de Visibilidad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCodigo;
+        private System.Windows.Forms.TextBox textBoxDescripcion;
+        private System.Windows.Forms.TextBox textBoxPPP;
+        private System.Windows.Forms.TextBox textBoxPorcentaje;
+        private System.Windows.Forms.Button buttonAlta;
+        private System.Windows.Forms.Button buttonLimpiar;
+    }
+}
---
+++ trunk/FrbaCommerce/FrbaCommerce/Abm Visibilidad/FormModificacionVisibilidad.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FrbaCommerce.Modelo;
+
+namespace FrbaCommerce.Abm_Visibilidad
+{
+    public partial class FormModificacionVisibilidad : Form
+    {
+        SistemManager cManager;
+        public FormModificacionVisibilidad(SistemManager cManager)
+        {
+            InitializeComponent();
+            this.cManager = cManager;
+            cManager.sqlAbmVisibilidad.Buscar(cManager, this.dataGridViewModificacionVisibilidad);
+            dataGridViewModificacionVisibilidad.Update();
+        }
+
+
+
+        private void dataGridViewRolFuncion_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            this.Hide();
+            if (this.Text.Equals("Modificacion Visibilidad"))
+            {
+
+                FormAltaVisibilidad formAltaVisibilidad = new FormAltaVisibilidad(cManager, true);
+                cManager.sqlAbmVisibilidad.cargarDatosDeModificacion(cManager, formAltaVisibilidad, this.dataGridViewModificacionVisibilidad.Rows[e.RowIndex].Cells[1].Value.ToString());
+                formAltaVisibilidad.ShowDialog();
+            }
+            else
+            {
+                FormBajaVisibilidad formBajaVisibilidad = new FormBajaVisibilidad(cManager);
+                cManager.sqlAbmVisibilidad.cargarDatosDeBaja(cManager, formBajaVisibilidad, this.dataGridViewModificacionVisibilidad.Rows[e.RowIndex].Cells[1].Value.ToString());
+                formBajaVisibilidad.ShowDialog();
+            }
+        }
+    }
+}
